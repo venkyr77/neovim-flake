63e41566975786d6601e0dd50733f0bcc9c80421
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-10 20:49:35 +0000

    add nvim-cmp, cmp-buffer, cmp-path

---
+++ nvim/lua/config/completion/init.lua
@@ -0,0 +1,17 @@
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = {
+    ["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<C-Space>"] = cmp.mapping.complete({}),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  },
+  sources = {
+    { name = "buffer" },
+    { name = "path" },
+  },
+})
