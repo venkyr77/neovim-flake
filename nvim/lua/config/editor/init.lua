1b8321dc70e6bea734812fca5d9a0f51766b26ba
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-10 15:10:23 +0000

    add snacks

--- nvim/lua/config/editor/init.lua
+++ nvim/lua/config/editor/init.lua
@@ -16,3 +16,52 @@ require("nvim-treesitter.configs").setup({
   modules = {},
   sync_install = false,
 })
+
+require("snacks").setup({
+  explorer = {
+    enabled = true,
+  },
+  indent = {
+    enabled = true,
+    scope = {
+      enabled = false,
+    },
+  },
+  notifier = {
+    enabled = true,
+  },
+  picker = {
+    enabled = true,
+    sources = {
+      explorer = {
+        hidden = true,
+        win = {
+          input = {
+            keys = {
+              ["<Esc>"] = "",
+            },
+          },
+          list = {
+            keys = {
+              ["<Esc>"] = "",
+            },
+          },
+        },
+      },
+      files = {
+        layout = {
+          fullscreen = true,
+        },
+      },
+      grep = {
+        layout = {
+          fullscreen = true,
+        },
+      },
+    },
+    ui_select = true,
+  },
+  words = {
+    enabled = true,
+  },
+})
