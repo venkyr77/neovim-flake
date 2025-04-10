046fdcff7aed04f398d5730bcb156ee1969d64b6
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-10 22:54:12 +0000

    add more opts

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,9 @@
+vim.opt.cursorline = true
 vim.opt.laststatus = 3
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.termguicolors = true
+vim.opt.undofile = true
