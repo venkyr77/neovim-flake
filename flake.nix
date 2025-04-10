f85e9f149fc1e05de19923e6e9226f77fcd30ef9
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-10 08:13:03 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    hercules-ci-effects = {
+      url = "github:hercules-ci/hercules-ci-effects";
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    mnw.url = "github:Gerg-L/mnw";
+    neovim-nightly = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        flake-parts.follows = "flake-parts";
+        git-hooks.follows = "git-hooks";
+        hercules-ci-effects.follows = "hercules-ci-effects";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    nixpkgs.url = "github:/NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = {
+    flake-parts,
+    mnw,
+    neovim-nightly,
+    nixpkgs,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {system, ...}: {
+        packages.default = mnw.lib.wrap nixpkgs.legacyPackages.${system} {
+          inherit (neovim-nightly.packages.${system}) neovim;
+          initLua = "require('config')";
+          plugins = [./nvim];
+        };
+      };
+    };
+}
