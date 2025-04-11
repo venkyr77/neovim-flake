ae2711066160ad376b1b9e081c5231a78a443728
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-11 13:39:50 +0000

    add noice suggested opts

--- nvim/lua/config/editor/init.lua
+++ nvim/lua/config/editor/init.lua
@@ -155,7 +155,22 @@ require("lualine").setup({
   },
 })
 
-require("noice").setup()
+require("noice").setup({
+  lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  presets = {
+    bottom_search = true,
+    command_palette = true,
+    inc_rename = false,
+    long_message_to_split = true,
+    lsp_doc_border = false,
+  },
+})
 
 require("nvim-autopairs").setup()
 
