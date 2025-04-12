194ff27b867b5832d729860d27c3ee0ecc0c23fb
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-12 08:03:14 +0000

    diagnostic: init

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -2,6 +2,14 @@ local lspconfig = require("lspconfig")
 local snacks = require("snacks")
 local wk = require("which-key")
 
+local virtual_lines_format = function(diagnostic)
+  return string.format("%s (%s)", diagnostic.message, diagnostic.source)
+end
+
+vim.diagnostic.config({
+  virtual_lines = { format = virtual_lines_format },
+})
+
 require("lazydev").setup()
 
 local client_capabilities = vim.tbl_deep_extend(
@@ -14,6 +22,30 @@ local client_capabilities = vim.tbl_deep_extend(
 local goto_picker_opts = {
   layout = { fullscreen = true },
 }
+
+local jump_to_diagnostic = function(count)
+  if not vim.b.current_min_severity then
+    vim.b.current_min_severity = vim.diagnostic.severity.HINT
+  end
+
+  vim.diagnostic.jump({
+    count = count,
+    float = false,
+    severity = { min = vim.b.current_min_severity },
+  })
+end
+
+local refresh_diagnostic_severity = function()
+  vim.diagnostic.config({
+    virtual_lines = {
+      format = virtual_lines_format,
+      severity = { min = vim.b.current_min_severity },
+    },
+    underline = {
+      severity = { min = vim.b.current_min_severity },
+    },
+  })
+end
 
 local on_attach = function(client, buffer)
   wk.add({
@@ -62,6 +94,59 @@ local on_attach = function(client, buffer)
         snacks.picker.lsp_type_definitions(goto_picker_opts)
       end,
       desc = "[t]ype definitions",
+    },
+    {
+      "<leader>l",
+      buffer = buffer,
+      desc = "+[l]sp",
+    },
+    {
+      "<leader>ld",
+      buffer = buffer,
+      desc = "+[d]iagnostics",
+    },
+    {
+      "<leader>lda",
+      buffer = buffer,
+      function()
+        vim.b.current_min_severity = vim.diagnostic.severity.HINT
+        refresh_diagnostic_severity()
+      end,
+      desc = "[a]ll",
+    },
+    {
+      "<leader>lde",
+      buffer = buffer,
+      function()
+        vim.b.current_min_severity = vim.diagnostic.severity.ERROR
+        refresh_diagnostic_severity()
+      end,
+      desc = "[e]rror",
+    },
+    {
+      "<leader>ldn",
+      buffer = buffer,
+      function()
+        jump_to_diagnostic(1)
+      end,
+      desc = "[n]ext",
+    },
+    {
+      "<leader>ldp",
+      buffer = buffer,
+      function()
+        jump_to_diagnostic(-1)
+      end,
+      desc = "[p]rev",
+    },
+    {
+      "<leader>ldt",
+      buffer = buffer,
+      function()
+        vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+      end,
+      desc = "[t]oggle",
+      icon = " ",
     },
   })
 
