2a423b75193cde9ad6d7612cff5e3d1ba4e82592
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-12 01:54:56 +0000

    lsp: keymaps and more features

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -1,3 +1,7 @@
+local lspconfig = require("lspconfig")
+local snacks = require("snacks")
+local wk = require("which-key")
+
 local client_capabilities = vim.tbl_deep_extend(
   "force",
   {},
@@ -5,12 +9,70 @@ local client_capabilities = vim.tbl_deep_extend(
   require("cmp_nvim_lsp").default_capabilities()
 )
 
-local lspconfig = require("lspconfig")
+local goto_picker_opts = {
+  layout = { fullscreen = true },
+}
+
+local on_attach = function(client, buffer)
+  wk.add({
+    {
+      "<leader>g",
+      buffer = buffer,
+      desc = "+[g]oto(lsp)",
+    },
+    {
+      "<leader>gd",
+      buffer = buffer,
+      function()
+        snacks.picker.lsp_definitions(goto_picker_opts)
+      end,
+      desc = "[d]efinitions",
+    },
+    {
+      "<leader>gD",
+      buffer = buffer,
+      function()
+        snacks.picker.lsp_declarations(goto_picker_opts)
+      end,
+      desc = "[D]eclarations",
+    },
+    {
+      "<leader>gi",
+      buffer = buffer,
+      function()
+        snacks.picker.lsp_implementations(goto_picker_opts)
+      end,
+      desc = "[i]mplementations",
+    },
+    {
+      "<leader>gr",
+      buffer = buffer,
+      function()
+        snacks.picker.lsp_references(goto_picker_opts)
+      end,
+      desc = "[r]eferences",
+    },
+    {
+      "<leader>gt",
+      buffer = buffer,
+      function()
+        snacks.picker.lsp_type_definitions(goto_picker_opts)
+      end,
+      desc = "[t]ype definitions",
+    },
+  })
+
+  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+    vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
+  end
+end
 
 lspconfig.lua_ls.setup({
   capabilities = client_capabilities,
+  on_attach = on_attach,
 })
 
 lspconfig.nixd.setup({
   capabilities = client_capabilities,
+  on_attach = on_attach,
 })
